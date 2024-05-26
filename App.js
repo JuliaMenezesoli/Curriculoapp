12ff0b4f337959589d208089753b1cd11102aad7
Júlia Menezes <106993629+JuliaMenezesoli@users.noreply.github.com>
2024-05-25 21:27:27 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+ <Image
+
+source={require('./assets/julia.jpg')} 
+ style={{ width: 200, height: 200 }} />
+
+      <Text>CURRICULO</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
