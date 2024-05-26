7586629a84a869f7debae5c3e2e027dd369c513a
Júlia Menezes <106993629+JuliaMenezesoli@users.noreply.github.com>
2024-05-26 17:48:40 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,17 +1,63 @@
+import 'react-native-gesture-handler';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ContatoScreen from './ContatoScreen'; // Certifique-se de que o caminho está correto
+import ExperienciaScreen from './ExperienciaScreen'; // Certifique-se de que o caminho está correto
+import FormacaoScreen from './FormacaoScreen'; // Certifique-se de que o caminho está correto
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('./assets/julia.jpg')}
+        style={{ width: 200, height: 200 }}
+      />
+      <Text>JÚLIA MENEZES</Text>
+      <Button
+        title="Ver detalhes"
+        onPress={() => navigation.navigate('Details')}
+      />
+      <Button
+        title="Contato"
+        onPress={() => navigation.navigate('Contato')}
+      />
+      <Button
+        title="Experiência"
+        onPress={() => navigation.navigate('Experiencia')}
+      />
+      <Button
+        title="Formação"
+        onPress={() => navigation.navigate('Formacao')}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Detalhes da Júlia Menezes</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
- <Image
-
-source={require('./assets/julia.jpg')} 
- style={{ width: 200, height: 200 }} />
-
-      <Text>CURRICULO</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Contato" component={ContatoScreen} />
+        <Stack.Screen name="Experiencia" component={ExperienciaScreen} />
+        <Stack.Screen name="Formacao" component={FormacaoScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -23,3 +69,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+
